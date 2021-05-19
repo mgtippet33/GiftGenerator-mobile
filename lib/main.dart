3aaf99e639e9423bc099fc12375a3b0e339492e9
Anastasiia <avzinovyeva62@gmail.com>
2021-05-20 00:49:37 +0300

    added ui for user account page, added part of premium logic, added model for history data

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:gift_generator/blocs/auth_bloc.dart';
 import 'package:gift_generator/pages/notification.dart';
 import 'package:provider/provider.dart';
@@ -9,6 +10,7 @@ import 'package:gift_generator/pages/loginPage.dart';
 import 'package:gift_generator/themeModel.dart';
 
 void main() async {
+  await GetStorage.init('MyStorage');
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(ChangeNotifierProvider(
