d3cf94f5892d88e4b12f693a676532e26b5a716d
Anastasiia <avzinovyeva62@gmail.com>
2021-05-21 01:03:38 +0300

    one more failed idea how to generate ui from list

--- lib/pages/cabinet.dart
+++ lib/pages/cabinet.dart
@@ -18,19 +18,19 @@ class Cabinet extends StatefulWidget {
 }
 
 class _CabinetState extends State<Cabinet> {
-  List _history = [];
+  List<UserHistoryModel> _history = List();
   final box = GetStorage('MyStorage');
 
   @override
   initState() {
-    if(box.read('isPremium') == null){
+    if (box.read('isPremium') == null) {
       box.write('isPremium', false);
     }
-    _history.add(UserHistoryModel('New year', "01/01", "music, gaming",
+    _history.add(new UserHistoryModel('New year', "01/01", "music, gaming",
         "https://bezkoder.com/dart-list/", "woman", 23));
-    _history.add(UserHistoryModel('Happy birthday', "01/01", "music",
+    _history.add(new UserHistoryModel('Happy birthday', "01/01", "music",
         "https://bezkoder.com/dart-list/", "woman", 23));
-    _history.add(UserHistoryModel(
+    _history.add(new UserHistoryModel(
         'New year 1',
         "01/02",
         "music, gaming, cooking",
@@ -63,7 +63,9 @@ class _CabinetState extends State<Cabinet> {
                         height: 90.0,
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.all(Radius.circular(50)),
-                          color: box.read('isPremium') == true ? Colors.deepOrangeAccent : Colors.white,
+                          color: box.read('isPremium') == true
+                              ? Colors.deepOrangeAccent
+                              : Colors.white,
                         ),
                         child: Image.asset("assets/account_img.png"),
                       ),
@@ -83,11 +85,12 @@ class _CabinetState extends State<Cabinet> {
                                   borderRadius:
                                       new BorderRadius.circular(10.0))),
                           onPressed: () => {
-                            if(box.read('isPremium') == false) {
-                              Navigator.of(context).pushReplacement(
-                                  MaterialPageRoute(
-                                      builder: (context) => PaymentPage()))
-                            }
+                            if (box.read('isPremium') == false)
+                              {
+                                Navigator.of(context).pushReplacement(
+                                    MaterialPageRoute(
+                                        builder: (context) => PaymentPage()))
+                              }
                           },
                           child: Row(children: [
                             Image.asset(
@@ -119,17 +122,20 @@ class _CabinetState extends State<Cabinet> {
                   ),
                 ),
               ),
+              ListView(
+                children: _getListElements(_history),
+              ),
               /*ListView.builder(
-                  itemCount: _history.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return HistoryBlock();
-                  },
+                itemCount: _history.length,
+                itemBuilder: (context, index) {
+                  return Text(_history[index].holidays);
+                },
               ),*/
-              HistoryBlock(),
+              /*HistoryBlock(),
               Padding(padding: EdgeInsets.only(bottom: 15)),
               HistoryBlock(),
               Padding(padding: EdgeInsets.only(bottom: 15)),
-              HistoryBlock(),
+              HistoryBlock(),*/
             ],
           ),
         ),
@@ -137,6 +143,15 @@ class _CabinetState extends State<Cabinet> {
       bottomNavigationBar: NavigationBar(),
     );
   }
+}
+
+List<Widget> _getListElements(List<UserHistoryModel> allNews) {
+  List<Widget> widgets = List();
+  for (var n in allNews) {
+    widgets.add(HistoryBlock());
+  }
+
+  return widgets;
 }
 
 class HistoryBlock extends StatelessWidget {
