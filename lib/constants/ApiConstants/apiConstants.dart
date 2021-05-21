d8e4fa73a14bdadac83d2126331b330e462b74b9
Matvii <72960397+mgtippet33@users.noreply.github.com>
2021-05-21 15:04:03 +0300

    Added api for registration

--- lib/constants/ApiConstants/apiConstants.dart
+++ lib/constants/ApiConstants/apiConstants.dart
@@ -1,3 +1,4 @@
 class ApiConstants {
-  static String register_url_local = 'http://127.0.0.1:8000/api/signUp/';
+  static String register_url = 'http://127.0.0.1:8000/api/signUp/';
+  static String getUser_url = 'http://127.0.0.1:8000/api/profile/';
 }
