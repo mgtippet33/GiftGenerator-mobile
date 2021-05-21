68a900f568949880aeb8f4b9726f1898fb7cce0f
Matvii <72960397+mgtippet33@users.noreply.github.com>
2021-05-21 15:04:03 +0300

    Added api for registration

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -4,14 +4,30 @@ import 'package:gift_generator/models/User.dart';
 import '../constants/ApiConstants/apiConstants.dart' as urls;
 
 class ApiManager {
-  Future<http.Response> simpleRegister(User user) async {
-    var url = Uri.parse(urls.ApiConstants.register_url_local);
+  Future<http.Response> register(User user) async {
+    var url = Uri.parse(urls.ApiConstants.register_url);
     Map<String, dynamic> responceBody = {
       "email": user.email,
       "password": user.password
+    };
+    var responce = await http.post(url, body: responceBody);
+    return responce;
+  }
+
+  Future<http.Response> googleRegister(User user) async {
+    var url = Uri.parse(urls.ApiConstants.register_url);
+    Map<String, dynamic> responceBody = {
+      "email": user.email,
+      "name": user.name
     };
     print(responceBody);
     var responce = await http.post(url, body: responceBody);
     return responce;
   }
+
+  // Future<http.Response> getUser() async {
+  //   var url = Uri.parse(urls.ApiConstants.getUser_url);
+  //   var responce = await http.post(url, body: {"userId" : userId});
+  //   return responce;
+  // }
 }
