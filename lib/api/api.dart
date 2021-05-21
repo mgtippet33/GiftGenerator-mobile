673cf999c5aba17b4d3ea8f4d8c38698e7c7158d
Matvii <72960397+mgtippet33@users.noreply.github.com>
2021-05-21 16:25:40 +0300

    Added api for login and google authorization

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -20,14 +20,34 @@ class ApiManager {
       "email": user.email,
       "name": user.name
     };
-    print(responceBody);
     var responce = await http.post(url, body: responceBody);
     return responce;
   }
 
-  // Future<http.Response> getUser() async {
-  //   var url = Uri.parse(urls.ApiConstants.getUser_url);
-  //   var responce = await http.post(url, body: {"userId" : userId});
-  //   return responce;
-  // }
+  Future<http.Response> signIn(String email, String password) async {
+    var url = Uri.parse(urls.ApiConstants.login_url);
+    Map<String, dynamic> responceBody = {
+      "email": email,
+      "password": password
+    };
+    var responce = await http.post(url, body: responceBody);
+    return responce;
+  }
+
+  Future<http.Response> googleLogin(String email) async {
+    var url = Uri.parse(urls.ApiConstants.login_url);
+    Map<String, dynamic> responceBody = {
+      "email": email
+    };
+    var responce = await http.post(url, body: responceBody);
+    return responce;
+  }
+
+  Future<http.Response> getUser(String token) async {
+    var url = Uri.parse(urls.ApiConstants.getUser_url);
+    final responce = await http.get(url, headers: {
+      'Authorization': 'Bearer $token',
+    });
+    return responce;
+  }
 }
