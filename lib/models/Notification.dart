b54408ed6eb11dfa0d882533a77dcead86458e73
Anastasiia <avzinovyeva62@gmail.com>
2021-05-23 03:17:54 +0300

    added base logic

--- lib/models/Notification.dart
+++ lib/models/Notification.dart
@@ -1,13 +1,4 @@
 import 'package:flutter/cupertino.dart';
-
-/*abstract class ListItem {
-  /// The title line to show in a list item.
-  Widget buildElement(BuildContext context);
-
-  /// The subtitle line, if any, to show in a list item.
-  //Widget buildSubtitle(BuildContext context);
-}*/
-
 class HolidayNotification{
   final  String daysDue;
   final  String holiday;
